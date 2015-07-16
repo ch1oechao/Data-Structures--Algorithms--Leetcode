cc8289e40f5eeb3be9578863890f9d85bba428d9
Wenjing Zhao <zchen9@users.noreply.github.com>
2015-07-16 16:51:34 +0800

    Create SortAlgorithm.js

---
+++ SortAlgorithm.js
@@ -0,0 +1,96 @@
+// 数组测试平台
+function CArray(numElements) {
+    this.dataStore = [];
+    this.pos = 0;
+    this.numElements = numElements;
+    this.insert = insert;
+    this.toString = toString;
+    this.clear = clear;
+    this.setData = setData;
+    this.swap = swap;
+    
+    this.bubbleSort = bubbleSort;
+    
+    for (var i = 0; i < numElements; ++i) {
+        this.dataStore[i] = i;
+    }
+}
+
+function setData() {
+    for (var i = 0; i < this.numElements; ++i) {
+        this.dataStore[i] = Math.floor(Math.random() * (this.numElements + 1));
+    }
+}
+
+function clear() {
+    for (var i = 0; this.dataStore.length; ++i) {
+        this.dataStore[i] = 0;
+    }
+}
+
+function insert(element) {
+    this.dataStore[this.pos++] = element;
+}
+
+function toString() {
+    var retstr = "";
+    for (var i = 0; i < this.dataStore.length; ++i) {
+        retstr += this.dataStore[i] + " ";
+        if (i > 0 & i % 10 == 0) {
+            retstr += "\n";
+        }
+    }
+    return retstr;
+}
+
+function swap(arr, index1, index2) {
+    var temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+
+
+/* ------------------------------- 基本排序方法 ------------------------------------- */
+
+// 冒泡排序
+function bubbleSort() {
+    var numElements = this.dataStore.length;
+    var temp;
+    for (var outer = numElements; outer >= 2; --outer) {
+        for (var inner = 0; inner <= outer - 1; ++inner) {
+            if (this.dataStore[inner] > this.dataStore[inner + 1]) {
+                swap(this.dataStore, inner, inner + 1);
+            }
+        }
+        //console.log(this.toString()); 查看过程
+    }
+}
+
+// test 
+var numElements = 10;
+var mynums = new CArray(numElements);
+mynums.setData();
+console.log(mynums.toString());
+mynums.bubbleSort();
+console.log(mynums.toString());
+
+// 9 9 10 0 8 0 7 2 4 3 
+// 0 0 2 3 4 7 8 9 9 10
+
+
+// 选择排序
+function selectionSort() {
+    var min,
+        temp;
+    for (var outer = 0; outer <= this.dataStore.length - 2; ++outer) {
+        min = outer;
+        for (var inner = outer + 1; inner <= this.dataStore.length - 1; ++inner) {
+            if (this.dataStore[inner] < this.dataStore[min]) {
+                min = inner;
+            }
+            swap(this.dataStore, outer, min);
+        }
+    }
+}
+
+ 
